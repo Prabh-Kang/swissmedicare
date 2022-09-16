f82388a06319c95cf15295989b0d16bcfa5122d6
Prabh-Kang <prbdpkng@gmail.com>
2022-09-16 08:38:29 +0530

    [ADDED]: Navbar

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import './HomePage.module.css'
+
+const HomePage = () => {
+  return (
+    <div>
+      Homepage
+    </div>
+  )
+}
+
+export default HomePage;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import './NavBar.module.css'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="company-name">
+        Swiss Medicare Pvt. Ltd.
+      </div>
+
+      <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>CONTACT US</li>
+      </ul>
+    </nav>
+  )
+}
+
+
+export default NavBar;
