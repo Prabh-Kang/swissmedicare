f451ae85817cf751850d1146eccd12e3ef605d7d
Prabh-Kang <prbdpkng@gmail.com>
2022-09-17 10:19:10 +0530

    [ADDED]: responsiveness to nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar/NavBar';
 import HomePage from './Pages/HomePage/HomePage';
 import './index.css';
+import HamburgerMenu from './components/HamburgerMenu/HamburgerMenu';
 
 function App() {
   return (
     <div>
+      <HamburgerMenu />
       <NavBar />
       <HomePage />
     </div>
